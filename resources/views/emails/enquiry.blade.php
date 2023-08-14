6a0ae3861f91ed2241f340ca0526dc9a1a91800d
Akhtar Parveen <akhujapbca@gmail.com>
2023-08-14 16:07:50 +0530

    enquiry layout corrections

--- resources/views/emails/enquiry.blade.php
+++ resources/views/emails/enquiry.blade.php
@@ -9,10 +9,10 @@
 
     <h3>Hello {{ $name }},</h3>
     <p>Thank you for your interest on {{ $type }} on report {{$reportName}}</p>
-    <p>Our team will get in touch with you soon. If there is any emergency you can call on +91 88399 07677 or drop a
-        email on "sales email"</p>
+    <p>Our team will get in touch with you soon. If there is any emergency you can call on +918149924059 or drop a
+        email on sales@factviewresearch.com</p>
     <p>Thank You,</p>
-    <p>FactViewReasearch</p>
+    <p>FactViewResearch</p>
 </body>
 
 </html>
