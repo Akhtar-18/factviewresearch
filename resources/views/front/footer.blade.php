029ca218f1b5e4b8c1097db5f7202c9fd42c9a71
Akhtar Parveen <akhujapbca@gmail.com>
2023-09-11 15:22:50 +0530

    Language script changes

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -205,7 +205,7 @@
 
     function setCookie(key, value, expiry) {
         var expires = new Date();
-        expires.setTime(expires.getTime() + (expiry 24 60 60 1000));
+        expires.setTime(expires.getTime() + (expiry  24  60  60  1000));
         document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
     }
 </script>
