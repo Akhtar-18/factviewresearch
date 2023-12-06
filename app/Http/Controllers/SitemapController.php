3e826a5dc953a256ce7b45f8e3deccc0f541f937
Kailash sahu <sahukailash110779@gmail.com>
2023-12-06 18:50:05 +0530

    site map add

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,16 +3,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
+use App\Models\CaseStudy;
+use App\Models\PressRelease;
+use App\Models\ReportCategoryModel;
 use Illuminate\Http\Response;
 use Spatie\Sitemap\SitemapGenerator;
+use App\Models\ReportsModel;
+use App\Models\ServicesModel;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 class SitemapController extends Controller
 {
+    // public function generate()
+    // {
+    //     SitemapGenerator::create(config('app.url'))->writeToFile(public_path('sitemap.xml'));
+
+    //     return response()->view('sitemap')->header('Content-Type', 'text/xml');
+    // }
+
+
     public function generate()
     {
-        SitemapGenerator::create(config('app.url'))->writeToFile(public_path('sitemap.xml'));
+       $pages=['/','about','who-we-are','why-chooose-us','refund','disclaimer','terms','privacy-policy','all-reports',
+       'clienttestimonials','all-press-releases','all-case-studies','partners','all-blogs','contact-us'];
 
-        return response()->view('sitemap')->header('Content-Type', 'text/xml');
+        $reports = ReportsModel::select('url','updated_at')->get(); 
+        $reportsCategory = ReportCategoryModel::select('cat_name','updated_at')->get(); 
+        $services = ServicesModel::select('slug','created_at')->get();
+        $blogs = Blog::select('url','created_at')->get();
+        $casestudies = CaseStudy::select('url','created_at')->get();
+        $pressReleases = PressRelease::select('url','created_at')->get();
+
+        return response()->view('front.sitemap', [
+            'reports'           => $reports,
+            'reportsCategory'   => $reportsCategory,
+            'pages'             => $pages,
+            'services'          => $services,
+            'blogs'             => $blogs,
+            'casestudies'       => $casestudies,
+            'pressReleases'     => $pressReleases
+        ])->header('Content-Type', 'text/xml');
     }
 }
 
--- resources/views/front/sitemap.blade.php
+++ resources/views/front/sitemap.blade.php
@@ -1,3 +1,68 @@
-<!-- resources/views/sitemap.blade.php -->
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 
-{!! file_get_contents(public_path('sitemap.xml')) !!}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    @foreach($pages as $page)
+    <url>
+        <loc>{{ url($page) }}</loc>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @endforeach
+
+    @foreach ($reportsCategory as $reportcate)
+    <url>
+        <loc>{{ url('/') }}/report-category/{{ gerenaretslug($reportcate->cat_name) }}</loc>
+        <lastmod>{{ $reportcate->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @endforeach
+
+    @foreach ($reports as $report)
+    <url>
+        <loc>{{ url('/') }}/report/{{ $report->url }}</loc>
+        <lastmod>{{ $report->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+
+    @endforeach
+
+    @foreach ($services as $service)
+    <url>
+        <loc>{{ url('/') }}/services/{{ $service->slug }}</loc>
+        <lastmod>{{ $service->created_at->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @endforeach
+
+    @foreach ($blogs as $blog)
+    <url>
+        <loc>{{ url('/') }}/services/{{ $blog->url }}</loc>
+        <lastmod>{{ $blog->created_at->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @endforeach
+
+    @foreach ($casestudies as $casestudy)
+    <url>
+        <loc>{{ url('/') }}/case-study/{{ $casestudy->url }}</loc>
+        <lastmod>{{ $casestudy->created_at->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @endforeach
+
+
+    @foreach ($pressReleases as $press)
+    <url>
+        <loc>{{ url('/') }}/press-release/{{ $press->url }}</loc>
+        <lastmod>{{ $press->created_at->tz('UTC')->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+    </url>
+    @endforeach
+
+</urlset>
