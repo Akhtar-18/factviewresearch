437dbebb38c424ff4a216dfc842755dec43780e2
Kailash sahu <sahukailash110779@gmail.com>
2023-12-14 19:34:41 +0530

    tawk to changes is done

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -190,7 +190,19 @@
         s1.setAttribute('crossorigin', '*');
         s0.parentNode.insertBefore(s1, s0);
     })();
-</script> -->
+</script>  -->
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64f61afda91e863a5c119a1a/1h9glislg';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
 <!--End of Tawk.to Script-->
 
 <script defer async type="text/javascript">
