fb8451c7ecda78744556a7bb1370069b81e6a2b2
Kailash sahu <sahukailash110779@gmail.com>
2023-12-25 19:02:06 +0530

    talk to add

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -152,18 +152,17 @@
 <script defer async type="text/javascript" src="{{ asset('front/quform/js/scripts.js') }}"></script>
 <script defer async type="text/javascript" src="{{ asset('front/js/custom.js') }}"></script>
 <!-- all js include end -->
-
-// <script type="text/javascript">
-// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-// (function(){
-// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-// s1.async=true;
-// s1.src='https://embed.tawk.to/64f61afda91e863a5c119a1a/1h9glislg';
-// s1.charset='UTF-8';
-// s1.setAttribute('crossorigin','*');
-// s0.parentNode.insertBefore(s1,s0);
-// })();
-// </script>
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64f61afda91e863a5c119a1a/1h9glislg';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
 <!--End of Tawk.to Script-->
 
 </body>
