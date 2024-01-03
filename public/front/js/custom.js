ec538c3a8f3f877a5708d5e4cd2275d41e3ce457
Kailash sahu <sahukailash110779@gmail.com>
2024-01-03 11:02:44 +0530

    custom js and main js changes

--- public/front/js/custom.js
+++ public/front/js/custom.js
@@ -1,14 +1,6 @@
-$(document).ready(function(){$(".animate-redirect a[href^='#']").click(function(t){t.preventDefault(),t=$($(this).attr("href")).offset().top,$("body, html").animate({scrollTop:t},1e3)}),$(".countup").counterUp({delay:50,time:2e3}),$("body").on("cut copy",function(t){t.preventDefault()})});
+$(".testimonial-style3").owlCarousel({loop:!1,responsiveClass:!0,nav:!1,dots:!0,margin:0,smartSpeed:900,responsive:{0:{items:1},600:{items:1},1e3:{items:1}}}),$(".carousel-style3 .owl-carousel").owlCarousel({loop:!0,dots:!1,nav:!1,autoplay:!0,smartSpeed:900,autoplayTimeout:5e3,responsiveClass:!0,autoplayHoverPause:!1,responsive:{0:{items:1,margin:0},768:{items:2,margin:20},992:{items:2,margin:30},1200:{items:3,margin:30}}}),$("#clients").owlCarousel({loop:!0,nav:!1,dots:!1,autoplay:!0,autoplayTimeout:3e3,responsiveClass:!0,autoplayHoverPause:!1,smartSpeed:900,responsive:{0:{items:2,margin:20},768:{items:3,margin:40},992:{items:4,margin:60},1200:{items:5,margin:80}}}),$(".slider-fade").owlCarousel({items:1,loop:!0,margin:0,autoplay:!0,smartSpeed:500,mouseDrag:!1,animateIn:"fadeIn",animateOut:"fadeOut"}),$(".services-grids").owlCarousel({loop:!0,nav:!1,responsiveClass:!0,dots:!1,autoplay:!0,smartSpeed:900,mouseDrag:!1,responsive:{0:{items:1,margin:15,mouseDrag:!0},768:{items:2,margin:20,mouseDrag:!0},1200:{items:3,margin:25,mouseDrag:!1}}}),$(".owl-carousel").owlCarousel({items:1,loop:!0,dots:!1,margin:0,autoplay:!0,smartSpeed:500}),$(".animate-redirect a[href^='#']").click(function(e){e.preventDefault(),e=$($(this).attr("href")).offset().top,$("body, html").animate({scrollTop:e},1e3)}),$(".countup").counterUp({delay:50,time:2e3}),$("body").on("cut copy",function(e){e.preventDefault()}),$.ajaxSetup({headers:{"X-CSRF-TOKEN":$('meta[name="csrf-token"]').attr("content")}});
 
-$(document).ready(function() {
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    });
-
-    $("body").on("submit",".ajaxformfileupload", function(event){
+$("body").on("submit",".ajaxformfileupload", function(event){
     event.preventDefault();
     // if($(this).parsley().isValid() ) {
         // alert(11);
@@ -52,8 +44,6 @@ $(document).ready(function() {
                formdata.find('button[type="submit"]').html(ButtonText);
            }
        });
-       //return false;
-//    }
 });
 
 $("body").on("submit",".contactform", function(event){
