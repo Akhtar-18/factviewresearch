9df19cccb6802a9cf36913c9f38f3a0c728f3026
Akhtar Parveen <akhujapbca@gmail.com>
2024-01-13 09:15:07 +0530

    Migration changes

--- database/migrations/2023_06_16_230659_create_reports_table.php
+++ database/migrations/2023_06_16_230659_create_reports_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->text('segment')->nullable();
             $table->text('methodology')->nullable();
             $table->string('publish_month',100)->nullable()->index();
-            $table->text('pages')->index()->nullable();
+            $table->string('pages')->index();
             $table->text('graphical_data')->nullable();
             $table->text('image')->nullable();
             $table->text('image_alt')->nullable();
