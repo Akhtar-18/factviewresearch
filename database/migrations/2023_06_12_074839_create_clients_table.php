09b01895dbd24eb3faf3384508822b9d19524388
Akhtar Parveen <akhujapbca@gmail.com>
2024-01-13 08:57:03 +0530

    Migration changes

--- database/migrations/2023_06_12_074839_create_clients_table.php
+++ database/migrations/2023_06_12_074839_create_clients_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('name',200)->nullable()->inde();
-            $table->text('image')->nullable()->index();
+            $table->string('name', 200)->nullable()->index();
+            $table->string('image')->index();
             $table->softDeletes();
             $table->timestamps();
         });
