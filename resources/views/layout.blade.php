4f1291782a236df91637f6b00b76a3e13722455a
Akhtar Parveen <akhujapbca@gmail.com>
2024-01-19 00:53:46 +0530

    Changes added

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -625,14 +625,14 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ url('') }}/admin/vendor/jquery/jquery.min.js"></script>
-    <script src="{{ url('') }}/admin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
     <!-- Core plugin JavaScript-->
     <!-- <script src="{{ url('') }}/admin/endor/jquery-easing/jquery.easing.min.js"></script> -->
 
     <!-- Custom scripts for all pages-->
-    <script src="{{ url('') }}/admin/js/sb-admin-2.min.js"></script>
+    <script src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
 
     <!-- Page level plugins -->
     <!-- <script src="{{ url('') }}/admin/vendor/chart.js/Chart.min.js"></script> -->
@@ -644,11 +644,11 @@
 
 
     <!-- Page level plugins -->
-    <script src="{{ url('') }}/admin/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="{{ url('') }}/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="{{ asset('admin/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 
     <!-- Page level custom scripts -->
-    <script src="{{ url('') }}/admin/js/demo/datatables-demo.js"></script>
+    <script src="{{ asset('/admin/js/demo/datatables-demo.js') }}"></script>
 
     <!-- <script>
         tinymce.init({
