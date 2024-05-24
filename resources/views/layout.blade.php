a98f9b2984f128aa487b9a7fe0350a5e5ee8063f
Akhtar Parveen <akhujapbca@gmail.com>
2024-05-25 01:39:00 +0530

    texteditor script changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -37,7 +37,7 @@
 
 
    <!-- <script src="https://cdn.tiny.cloud/1/a9aynqzsx35zq4rrj8kbql2jq9chhsi708mnjcgt97gqzzy0/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>-->
-   <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+   <script src="https://cdn.tiny.cloud/1/qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
 
     <style type="text/css">
         .tox .tox-statusbar__text-container {
